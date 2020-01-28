07898a7892239dd92196c9dc7a9fdb6bdce75c77
Owen Roberts <oroberts@bmcc.cuny.edu>
2020-01-27 19:09:15 -0500

    adds characters sketch

---
+++ characters/sketch.js
@@ -0,0 +1,14 @@
+/*
+	characters sketch
+	1.27.2020
+*/
+
+// runs once to set up browser
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+// runs 60fps draws graphics
+function draw() {
+	background('lightblue');
+}
