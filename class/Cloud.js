618334beeeccaf88539ae18ad7c42e0981419b0f
owenbmcc <oroberts@bmcc.cuny.edu>
2020-08-26 16:20:42 -0400

    s20 new page

--- class/Cloud.js
+++
@@ -1,9 +0,0 @@
-class Cloud extends Thing {
-	update() {
-		this.x += 1;
-
-		if (this.x > width) {
-			this.x = -this.img.width;
-		}
-	}
-}
--- user_interface/Cloud.js
+++
@@ -1,9 +0,0 @@
-class Cloud extends Thing {
-	update() {
-		this.x += 2;
-
-		if (this.x > width) {
-			this.x = -this.img.width;
-		}
-	}
-}
