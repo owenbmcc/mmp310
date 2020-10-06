71c67e807d2cdf87d2d4ec31a2590d359f794b97
owenbmcc <oroberts@bmcc.cuny.edu>
2020-10-06 12:32:29 -0400

    Revert "fix typo"
    This reverts commit fe54ed1982012762bea27c6fd233966df3da40a1.

--- project1-6/GameObject.js
+++ project1-6/GameObject.js
@@ -15,7 +15,7 @@ class GameObject {
 		if (this.x - this.width/2 < other.x + other.width/2 &&
 			this.x + this.width/2 > other.x - other.width/2 &&
 			this.y - this.height/2 < other.y + other.height/2 &&
-			this.y + this.height/2 > other.y - other.height/2) {
+			this.y +this.height/2 > other.y - other.height/2) {
 			return true;
 		} else {
 			return false;
